e9cf33f1a1346daa651f6ba9d1618f79f7fc858b
Дмитрий Коптик <43681022+DmitriyKoptik@users.noreply.github.com>
2020-08-05 23:14:21 +0300

    first real try

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,49 +2,64 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var textView:TextView
+    private lateinit var textView2:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // setOnClickListener на текущую активити
-        buttonMain.setOnClickListener() {
-            val messageTemp = textViewMain.getText().toString()
-            textViewMain.setText(textViewMain2.getText().toString())
-            textViewMain2.setText(messageTemp)
+        val button = findViewById<Button>(R.id.buttonMain)
+        textView = findViewById<TextView>(R.id.textViewMain)
+        textView2 = findViewById<TextView>(R.id.textViewMain2)
 
-            val colorTemp = textViewMain.getBackground()
-            textViewMain.setBackground(textViewMain2.getBackground())
-            textViewMain2.setBackground(colorTemp)
+        // сделать имплемент OnClickListener на текущую активити
+        button.setOnClickListener(this)
+
+        // анонимный класс
+        textViewMain.setOnClickListener() {
+           val messageTemp = textViewMain.getText().toString()
+           textViewMain.setText(textViewMain2.getText().toString())
+           textViewMain2.setText(messageTemp)
+
+           val colorTemp = textViewMain.getBackground()
+           textViewMain.setBackground(textViewMain2.getBackground())
+           textViewMain2.setBackground(colorTemp)
         }
 
-        // анонимный класс. Тут я словил проблему с unresolved reference
-       textViewMain.setOnClickListener(new View.OnClickListener() {
-            fun onClick(View v) {
-                String messageTemp = textViewMain.getText().toString()
-                textViewMain.setText(textViewMain2.getText().toString())
-                textViewMain2.setText(messageTemp)
 
-                String colorTemp = textViewMain.getBackground()
-                textViewMain.setBackground(textViewMain2.getBackground())
-                textViewMain2.setBackground(colorTemp)
-           })
-
-           // Из-за прошлого варика до layouta не добрался, пока оставил только первую имплементацию
-        textViewMain2.setOnClickListener() {
-            val messageTemp = textViewMain.getText().toString()
-            textViewMain.setText(textViewMain2.getText().toString())
-            textViewMain2.setText(messageTemp)
-
-            val colorTemp = textViewMain.getBackground()
-            textViewMain.setBackground(textViewMain2.getBackground())
-            textViewMain2.setBackground(colorTemp)
-        }
 
     }
 
+    override fun onClick(v: View?) {
+        val messageTemp = textViewMain.getText().toString()
+        textView.setText(textViewMain2.getText().toString())
+        textView2.setText(messageTemp)
 
+        val colorTemp = textViewMain.getBackground()
+        textView.setBackground(textViewMain2.getBackground())
+        textView2.setBackground(colorTemp)
+    }
 
+        // layout xml
+    fun clickResponse(view: View) {
+        val messageTemp = textViewMain.getText().toString()
+        textViewMain.setText(textViewMain2.getText().toString())
+        textViewMain2.setText(messageTemp)
+
+        val colorTemp = textViewMain.getBackground()
+        textViewMain.setBackground(textViewMain2.getBackground())
+        textViewMain2.setBackground(colorTemp)
+    }
 }
+
+
+
